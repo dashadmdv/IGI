7c6b40ad54b9cdb1c60a999e1348da01f7163bc9
dasha.dmdv <apushkins7@gmail.com>
2023-03-28 19:25:05 +0300

    add: abbreviations and regular expressions

---
+++ Lab2/task1/src/constants.py
@@ -0,0 +1,16 @@
+SENTENCE_PATTERN = r"[.!?][^.!?]"
+NON_DECLARATIVE_PATTERN = r"[!?][^!?]"
+WORD_PATTERN = r'\b[a-zA-Z\d]+\b'
+NUMBER_PATTERN = r"\b[0-9]+\b\s*"
+
+ONE_WORD_ABBREVIATIONS = [
+    'etc.', 'vs.', 'jr.', 'sr.', 'mr.', 'ms.', 'mrs.', 'smb.', 'smth.', 'adj.', 'prep.', 'pp.', 'par.', 'ex.',
+    'pl.', 'edu.', 'appx.', 'sec.', 'gm.', 'cm.', 'yr.', 'jan.', 'feb.', 'mar.',
+    'apr.', 'jun.', 'jul.', 'aug.', 'sep.', 'oct.', 'nov.', 'dec.', 'mon.', 'tue.', 'wed.', 'thu', 'fri.', 'sat.',
+    'sun.']
+
+TWO_WORDS_ABBREVIATIONS = [
+    'e.g.', 'p.m.', 'a.m.', 'U.K.', 'p.p.', 'i.e.', 'U.S.',
+    'p.p', 't.p.', 'v.t.', 'v.i.']
+
+THREE_WORDS_ABBREVIATIONS = ['v.i.p.', 'p.p.s.']
