8da372d351a314a2065e339d69c9cbf5250029e3
dashadmdv <apushkins7@gmail.com>
2023-05-23 22:36:45 +0300

    add: json dumps

--- Lab3/main.py
+++ Lab3/main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('Help :(')
     print('sos')
+    print('sorry didn\'t have time to commit normally')
 
 
 # Press the green button in the gutter to run the script.
